99556a094d53579b401f7c2267b8b09dd9dd0f05
amadoutang84 <amadoutangara824@gmail.com>
2023-03-17 16:58:19 +0000

    la distane et le temps avant la destion

---
+++ temps et distance.R
@@ -0,0 +1,14 @@
+le temps et la distanced qu ils font avant d arriver à la destination 
+
+dist = c(2, 4, 5, 3, 15, 10, 4, 12, 8)
+temp = c(15, 25, 25, 50, 45, 10, 38, 55, 30)
+nom = c("Lyn", "Issa", "Ami", "Madou", "Fana", "oumou", "Kadja", "issou", "Ali")
+genre = c("F", "H", "F", "H", "F", "F", "F", "H", "H")
+
+plot( dist, temp, main = "distance vs. temps", 
+    xlab = "distance (en km)", ylab = "temps (en mn)", col="red", pch=15 )
+
+
+text(dist, temp+1, labels = nom, col = "blue", )
+
+text(dist, temp, genre,, pos=1, col = "darkgreen")
